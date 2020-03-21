007ede3e7b60684c1b8e5118b5478eeb621a7f1b
Walczi123 <patryk.wal@onet.pl>
2020-03-21 12:27:05 +0100

    TheGame

---
+++ The Game/GameMaster/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameMaster
+{
+    interface IGameMaster
+    {
+        void GenerateBoard();
+        void UpdateBoard(string[] );
+        void SendMessage(string message);
+
+        string Lis();
+        void MessageManager();
+    }
+    class GM
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
