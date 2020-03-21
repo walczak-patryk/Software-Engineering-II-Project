75e265a8209f68edc09a267e6665c56b93ecafa7
Walczi123 <patryk.wal@onet.pl>
2020-03-21 13:22:26 +0100

    point

--- The Game/GameMaster/Board.cs
+++ The Game/GameMaster/Board.cs
@@ -5,19 +5,46 @@ using System.Windows;
 
 namespace GameMaster
 {
-
-
     interface IBoard
     {
         void UpdateBoard(string[] message);
         void PieceInformation();
         void GeneratePiece();
-        int DisplayManhattanDistance(Point point);
+        int DisplayManhattanDistance(int x, int y);
         bool FieldOccupied();
         void PlacePiece();
     }
 
-    class Board
+    class Board : IBoard
     {
+        public int DisplayManhattanDistance(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool FieldOccupied()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GeneratePiece()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PieceInformation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PlacePiece()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateBoard(string[] message)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
