c67124a1000561590fdfeee2d192cd658fb8985e
Patryk <patryk.wal@onet.pl>
2020-03-30 18:20:24 +0200

    position class fixing

--- The Game/GameMaster/Positions/Position.cs
+++ The Game/GameMaster/Positions/Position.cs
@@ -5,8 +5,24 @@
         private int x;
         private int y;
 
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
         public void ChangePosition(Direction direction)
         {
+            switch (direction)
+            {
+                case Direction.Left: this.x -= 1;
+                    break;
+                case Direction.Right: this.x += 1;
+                    break;
+                case Direction.Up:  this.y += 1;
+                    break;
+                case Direction.Down: this.y -= 1;
+                    break;
+            }
         }
     }
 }
