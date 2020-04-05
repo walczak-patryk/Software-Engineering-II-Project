26b9a5c49e8a3db30a2a082efb39ef16e2facbb0
zbartkus <zbartkuss3150@gmail.com>
2020-04-05 18:58:45 +0200

    Player AI added

--- The Game/GameServices/GameServices/Player.cs
+++ The Game/GameServices/GameServices/Player.cs
@@ -2,6 +2,7 @@
 using GameMaster.Cells;
 using GameMaster.Positions;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
 
@@ -16,11 +17,14 @@ namespace GameMaster
         public Position position;
         public bool piece;
         public bool pieceIsSham;
+        public bool isDiscovered;
         public Board board;
         public ActionType lastAction;
         public string guid;
         private PlayerState state;
         private string pipe;
+
+        public int turnsSinceDiscover;
 
         public static void Main()
         {
@@ -40,7 +44,9 @@ namespace GameMaster
             this.piece = false;
             this.pieceIsSham = false;
             this.board = new Board(0,0,0);
-            this.guid = Team.getColor().ToString()[0] + Id.ToString(); 
+            this.guid = Team.getColor().ToString()[0] + Id.ToString();
+            this.turnsSinceDiscover = 100;
+            this.isDiscovered = false;
         }
 
         public void Move(Direction x, Direction y)
@@ -244,12 +250,15 @@ namespace GameMaster
                 this.piece = false;
                 this.pieceIsSham = false;
             }
+            this.isDiscovered = false;
+
         }
 
         private void TestPiece()
         {
             if(this.piece==true)
             {
+                this.isDiscovered = true;
                 return;
             }
             if(this.pieceIsSham==false)
@@ -267,6 +276,141 @@ namespace GameMaster
                 {
                     Cell cell = board.GetCell(position);
                     cell.SetCellState(CellState.Goal);
+                }
+            }
+        }
+
+
+        void AIMove() //simple AI for player
+        {
+            Random rand = new Random();
+            if (piece == false)
+            {
+                if (turnsSinceDiscover > 0)
+                    Discover();
+                else
+                {
+                    List<int> distances = new List<int>();
+                    for (int j = -1; j <= 1; j++)
+                    {
+                        for (int i = -1; i <= 1; i++)
+                        {
+                            if (position.x + i < 0 || position.y + j < 0 || position.x + i > board.boardWidth || position.y + j > board.boardHeight)
+                                distances.Add(Math.Max(board.boardWidth, board.boardHeight));
+                            else
+                                distances.Add(board.cellsGrid[position.x + i, position.y + j].GetDistance());
+                        }
+                    }
+                    int min = distances[0];
+                    int dir = 0;
+                    for (int i = 1; i < distances.Count; ++i)
+                    {
+                        if (distances[i] < min)
+                        {
+                            min = distances[i];
+                            dir = i;
+                        }
+
+                        switch (dir)
+                        {
+                            case 0:
+                                {
+                                    if (rand.Next() % 2 == 0)
+                                        Move(Direction.Left);
+                                    else
+                                        Move(Direction.Up);
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    Move(Direction.Up);
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    if (rand.Next() % 2 == 0)
+                                        Move(Direction.Right);
+                                    else
+                                        Move(Direction.Up);
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    Move(Direction.Left);
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    TakePiece();
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    Move(Direction.Right);
+                                    break;
+                                }
+                            case 6:
+                                {
+                                    if (rand.Next() % 2 == 0)
+                                        Move(Direction.Left);
+                                    else
+                                        Move(Direction.Down);
+                                    break;
+                                }
+                            case 7:
+                                {
+                                    Move(Direction.Down);
+                                    break;
+                                }
+                            case 8:
+                                {
+                                    if (rand.Next() % 2 == 0)
+                                        Move(Direction.Right);
+                                    else
+                                        Move(Direction.Down);
+                                    break;
+                                }
+                            default: break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (!isDiscovered)
+                {
+                    TestPiece();
+                }
+                else
+                {
+                    if (team.getColor() == TeamColor.Blue)
+                    {
+                        if (position.y < board.taskAreaHeight + board.goalAreaHeight)
+                            Move(Direction.Up);
+                        else
+                        {
+                            if (board.GetCell(position).GetCellState() == CellState.Goal)
+                            {
+                                Move(Direction.Up + rand.Next() % 3);
+                            }
+                            else
+                                PlacePiece();
+                        }
+                    }
+                    else
+                    {
+                        if (position.y > board.goalAreaHeight)
+                            Move(Direction.Down);
+                        else
+                        {
+                            if (board.GetCell(position).GetCellState() == CellState.Goal)
+                            {
+                                Move(Direction.Up + rand.Next() % 3);
+                            }
+                            else
+                                PlacePiece();
+                        }
+                    }
                 }
             }
         }
