42461b486cdd6a04c1d0318391b701e112d5bdb0
szymczakf <filip.p.szymczak@gmail.com>
2020-05-11 12:00:38 +0200

    GM connection messages

---
+++ The Game/CommunicationServerLibrary/Messages/ConnectGMMsg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GameMaster;
+
+namespace CommunicationServerLibrary.Messages
+{
+    class ConnectGMMsg : Message
+    {
+        public string portNumber;
+
+        public ConnectGMMsg(string portNumber) : base("connect GM")
+        {
+            this.portNumber = portNumber;
+        }
+    }
+}
