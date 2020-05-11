8319ed4178db635485f51e7c7809ccfd80c93014
martin <maartinmxxx@gmail.com>
2020-05-11 13:33:22 +0200

    End message

---
+++ The Game/CommunicationServerLibrary/Messages/GameEndMsg.cs
@@ -0,0 +1,13 @@
+﻿using GameMaster;
+
+namespace CommunicationServerLibrary.Messages
+{
+    public class GameEndMsg : Message
+    {
+        public TeamColor result;
+        public GameEndMsg(TeamColor result) : base("end")
+        {
+            this.result = result;
+        }
+    }
+}
