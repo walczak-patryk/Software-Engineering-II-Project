6be82c631c7e01808ce55162d633e0794033cf41
zalewskim2 <m.zalewski2@student.mini.pw.edu.pl>
2020-05-25 17:15:07 +0200

    Validate IP and port

--- The Game/GameServices/GameServices/StartOfPlayer.cs
+++ The Game/GameServices/GameServices/StartOfPlayer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace GameMaster
@@ -19,8 +20,18 @@ namespace GameMaster
             string start = "";
             Console.Write("Please type Ip address of server\n# ");
             string ip = Console.ReadLine();
+            while (!ValidateIP(ip))
+            {
+                Console.Write("Please type Ip address of server\n# ");
+                ip = Console.ReadLine();
+            }
             Console.Write("Please type port number of server\n# ");
             string port = Console.ReadLine();
+            while (!ValidatePort(port))
+            {
+                Console.Write("Please type port number of server\n# ");
+                port = Console.ReadLine();
+            }
             while (start != "start")
             {
                 Console.Write("type \"start\" to connect to the game\n# ");
@@ -34,6 +45,44 @@ namespace GameMaster
             {
                 Console.Write("type \"exit\" to shutdown or close console\n# ");
                 testString = Console.ReadLine();
+            }
+        }
+
+        private static bool ValidateIP(string ip)
+        {
+            try
+            {
+                IPAddress.Parse(ip);
+                return true;
+            }
+            catch (ArgumentNullException)
+            {
+                Console.Write("Exception: IP is null.\n");
+                return false;
+            }
+            catch (FormatException)
+            {
+                Console.Write("Exception: Wrong IP format.\n");
+                return false;
+            }
+        }
+
+        private static bool ValidatePort(string port)
+        {
+            try
+            {
+                int valPort = Convert.ToInt32(port);
+                return valPort > 0;
+            }
+            catch (FormatException)
+            {
+                Console.Write("Exception: Wrong port format.\n");
+                return false;
+            }
+            catch (OverflowException)
+            {
+                Console.Write("Exception: Overflow.\n");
+                return false;
             }
         }
     }
