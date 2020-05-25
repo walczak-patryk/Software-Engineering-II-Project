192da89af4c8b6a8f486b1ece67d6539ba90e0f3
szymczakf <filip.p.szymczak@gmail.com>
2020-05-25 16:09:54 +0200

    Default player connection

--- The Game/GameServices/GameServices/StartOfPlayer.cs
+++ The Game/GameServices/GameServices/StartOfPlayer.cs
@@ -19,8 +19,12 @@ namespace GameMaster
             string start = "";
             Console.Write("Please type Ip address of server\n# ");
             string ip = Console.ReadLine();
+            if (ip == "")
+                ip = "127.0.0.1";
             Console.Write("Please type port number of server\n# ");
             string port = Console.ReadLine();
+            if (port == "")
+                port = "13000";
             while (start != "start")
             {
                 Console.Write("type \"start\" to connect to the game\n# ");
